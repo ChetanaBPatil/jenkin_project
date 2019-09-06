b2f0f3e6698685b2dbe4b18057a3adb71df9a480
CB57565 <CB57565@TRNW57SLPC0112.ind.zensar.com>
2019-09-06 12:40:10 +0530

    My first jenkin project.

---
+++ Demo/src/com/Display.java
@@ -0,0 +1,31 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Display {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		//c=new Sample1();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		//c=null;
+	}
+
+	@Test
+	void testCount() {
+		System.out.println("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		System.out.println("Not yet implemented");
+	}
+
+}
---
+++ Demo/src/com/Sample1.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Sample1 {
+	
+	public void count()
+	{
+		System.out.println("hiii");
+	}
+
+	public static void main(String[] args) {
+		
+		Sample1 s1=new Sample1();
+		s1.count();
+		
+	}
+
+}
